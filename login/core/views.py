e6f786b75ea3982a1d936f8446fb2389b136b6c5
CamiloBCrep <141046727+CamiloBCrep@users.noreply.github.com>
2024-05-25 17:16:42 -0400

    Configuración e implementación de nuevos templates

--- login/core/views.py
+++ login/core/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+def home(request):
+    title = "inicio"
+    data={
+        "title":title
+    }
+    return render(request, 'core/home.html', data)
+
+
+def login(request):
+    title = "Inicio de sesión"
+
+    data = {
+        "title":title,
+    }
+    return render(request, 'core/login.html',data)
+def recovery(request):
+    title = "Recuperación de contraseña"
+    data = {
+        "title":title,
+    }
+    return render(request, 'core/recovery.html', data)
 # Create your views here.
