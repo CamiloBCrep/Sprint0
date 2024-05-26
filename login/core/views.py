bef48a6dcdfeb00b85f5edea014b0d5a6ff2af6f
Camilo Bilbao <camilo.bilbao@sansano.usm.cl>
2024-05-26 16:18:53 -0400

    crispy added

--- login/core/views.py
+++ login/core/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
 
 def home(request):
     title = "inicio"
@@ -7,18 +10,21 @@ def home(request):
     }
     return render(request, 'core/home.html', data)
 
-
 def login(request):
     title = "Inicio de sesión"
 
     data = {
         "title":title,
     }
-    return render(request, 'core/login.html',data)
+    return render(request, 'registration/login.html',data)
+
 def recovery(request):
     title = "Recuperación de contraseña"
     data = {
         "title":title,
     }
     return render(request, 'core/recovery.html', data)
-# Create your views here.
+
+def exit(request):
+    logout(request)
+    return redirect('home')
--- login/login/settings.py
+++ login/login/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -104,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-us'
 
 TIME_ZONE = 'UTC'
 
@@ -118,7 +122,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
